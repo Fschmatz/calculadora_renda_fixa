5d58808fe2e56ce0683ae897ba3520d6f0362c94
Fernando <fschmatz@hotmail.com>
2022-02-08 22:08:01 -0300

    v 0.3.0

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -11,7 +11,6 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
-
   double txtFieldSpacing = 25;
   TextStyle labelStyle =
       const TextStyle(fontSize: 16, fontWeight: FontWeight.w600);
@@ -26,18 +25,25 @@ class _HomeState extends State<Home> {
 
   @override
   void initState() {
+    valor.text = '10000';
+    periodo.text = '12';
+    taxaDi.text = '10';
+    taxaSelic.text = '10';
+    cdb.text = '10';
+    lciLca.text = '10';
+
     super.initState();
   }
 
-  void calcularResultado(){
+  void calcularResultado() {
+    double selic = double.parse(taxaSelic.text);
+    double porcCDB = double.parse(cdb.text);
+    double porcLci = double.parse(lciLca.text);
 
     double valorInicial = double.parse(valor.text);
-   // double valorFinal = double.parse(valor.text) * 2;
     double periodoInv = double.parse(periodo.text);
-    /*double di =  double.parse(taxaDi.text);
-    double selic =  double.parse(taxaSelic.text);
-    double porcCDB =  double.parse(cdb.text);
-    double porcLc =  double.parse(lciLca.text);*/
+    double di = double.parse(taxaDi.text);
+
     double impRenda = 0;
     bool ir = true;
 
@@ -51,15 +57,17 @@ class _HomeState extends State<Home> {
       impRenda = 15;
     }
 
-
-    _resultado += "Poupança: "+calcularPoupanca(valorInicial,periodoInv).toStringAsFixed(2)+"\n";
-    //_resultado += "LCI / LCA: "+calcularLci(valorInicial,periodoInv).toStringAsFixed(2)+"\n";
+    _resultado += "Poupança: " +
+        calcularPoupanca(valorInicial, periodoInv).toStringAsFixed(2) +
+        "\n";
+    _resultado += "LCI / LCA: " +
+        calcularLci(valorInicial, periodoInv, di, impRenda,porcLci).toStringAsFixed(2) +
+        "\n\n";
 
     setState(() {
       _resultado;
     });
   }
-
 
 /*calcularImpostoRenda(){
 
@@ -68,223 +76,237 @@ class _HomeState extends State<Home> {
     var valorFinal = valorInicial + (valorFinal - valorInicial) / (1 - (faixaIr / 100));
   }*/
 
+  void loseFocus() {
+    FocusScopeNode currentFocus = FocusScope.of(context);
+    if (!currentFocus.hasPrimaryFocus) {
+      currentFocus.unfocus();
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: const Text('Calculadora Renda Fixa'),
-          actions: [
-            IconButton(
-                icon: const Icon(
-                  Icons.settings_outlined,
-                ),
-                onPressed: () {
-                  Navigator.push(
-                      context,
-                      MaterialPageRoute<void>(
-                        builder: (BuildContext context) => PgConfigs(),
-                        fullscreenDialog: true,
-                      ));
-                }),
-          ],
-        ),
-        body: ListView(
-          padding: const EdgeInsets.symmetric(horizontal: 16),
-          shrinkWrap: true,
-          children: [
-            const SizedBox(
-              height: 10,
-            ),
-            TextField(
-              inputFormatters: <TextInputFormatter>[
-                FilteringTextInputFormatter.allow(
-                    RegExp(r'^(\d+)?\.?\d{0,2}'))
-              ],
-              keyboardType: const TextInputType.numberWithOptions(decimal: true),
-              minLines: 1,
-              maxLines: 1,
-              maxLength: 10,
-              maxLengthEnforcement: MaxLengthEnforcement.enforced,
-              controller: valor,
-              decoration: const InputDecoration(
-                labelText: "Valor da Aplicação",
-                counterText: "",
-              ),
-              textAlign: TextAlign.end,
-              style: const TextStyle(
-                fontSize: 16,
-              ),
-              onEditingComplete: () => {},
-            ),
-            SizedBox(
-              height: txtFieldSpacing,
-            ),
-            TextField(
-              inputFormatters: <TextInputFormatter>[
-                FilteringTextInputFormatter.allow(
-                    RegExp(r'^(\d+)?\.?\d{0,2}'))
-              ],
-              keyboardType: const TextInputType.numberWithOptions(decimal: true),
-              minLines: 1,
-              maxLines: 1,
-              maxLength: 10,
-              maxLengthEnforcement: MaxLengthEnforcement.enforced,
-              controller: periodo,
-              decoration: const InputDecoration(
-                labelText: "Período (Meses)",
-                counterText: "",
-              ),
-              textAlign: TextAlign.end,
-              style: const TextStyle(
-                fontSize: 16,
-              ),
-              onEditingComplete: () => {},
-            ),
-            SizedBox(
-              height: txtFieldSpacing,
-            ),
-            Row(
-              children: [
-                Flexible(
-                  child: TextField(
-                    inputFormatters: <TextInputFormatter>[
-                      FilteringTextInputFormatter.allow(
-                          RegExp(r'^(\d+)?\.?\d{0,2}'))
-                    ],
-                    keyboardType: const TextInputType.numberWithOptions(decimal: true),
-                    minLines: 1,
-                    maxLines: 1,
-                    maxLength: 10,
-                    maxLengthEnforcement: MaxLengthEnforcement.enforced,
-                    controller: taxaDi,
-                    decoration: const InputDecoration(
-                      labelText: "Taxa DI - % ao Ano",
-                      counterText: "",
-                    ),
-                    textAlign: TextAlign.end,
-                    style: const TextStyle(
-                      fontSize: 16,
-                    ),
-                    onEditingComplete: () => {},
-                  ),
-                ),
-                SizedBox(
-                  width: txtFieldSpacing,
-                ),
-                Flexible(
-                  child: TextField(
-                    inputFormatters: <TextInputFormatter>[
-                      FilteringTextInputFormatter.allow(
-                          RegExp(r'^(\d+)?\.?\d{0,2}'))
-                    ],
-                    keyboardType: const TextInputType.numberWithOptions(decimal: true),
-                    minLines: 1,
-                    maxLines: 1,
-                    maxLength: 10,
-                    maxLengthEnforcement: MaxLengthEnforcement.enforced,
-                    controller: taxaSelic,
-                    decoration: const InputDecoration(
-                      labelText: "Taxa Selic - % ao Ano",
-                      counterText: "",
-                    ),
-                    textAlign: TextAlign.end,
-                    style: const TextStyle(
-                      fontSize: 16,
-                    ),
-                    onEditingComplete: () => {},
-                  ),
-                ),
-              ],
-            ),
-           
-            SizedBox(
-              height: txtFieldSpacing,
-            ),
-            Row(
-              children: [
-                Flexible(
-                  child: TextField(
-                    inputFormatters: <TextInputFormatter>[
-                      FilteringTextInputFormatter.allow(
-                          RegExp(r'^(\d+)?\.?\d{0,2}'))
-                    ],
-                    keyboardType: const TextInputType.numberWithOptions(decimal: true),
-                    minLines: 1,
-                    maxLines: 1,
-                    maxLength: 10,
-                    maxLengthEnforcement: MaxLengthEnforcement.enforced,
-                    controller: cdb,
-                    decoration: const InputDecoration(
-                      labelText: "% CDB / LC",
-                      counterText: "",
-                    ),
-                    textAlign: TextAlign.end,
-                    style: const TextStyle(
-                      fontSize: 16,
-                    ),
-                    onEditingComplete: () => {},
-                  ),
-                ),
-                SizedBox(
-                  width: txtFieldSpacing,
-                ),
-                Flexible(
-                  child: TextField(
-                    inputFormatters: <TextInputFormatter>[
-                      FilteringTextInputFormatter.allow(
-                          RegExp(r'^(\d+)?\.?\d{0,2}'))
-                    ],
-                    keyboardType: const TextInputType.numberWithOptions(decimal: true),
-                    minLines: 1,
-                    maxLines: 1,
-                    maxLength: 10,
-                    maxLengthEnforcement: MaxLengthEnforcement.enforced,
-                    controller: lciLca,
-                    decoration: const InputDecoration(
-                      labelText: "% LCI / LCA",
-                      counterText: "",
-                    ),
-                    textAlign: TextAlign.end,
-                    style: const TextStyle(
-                      fontSize: 16,
-                    ),
-                    onEditingComplete: () => {},
-                  ),
-                ),
-              ],
-            ),
-
-
-            const SizedBox(
-              height: 40,
-            ),
-            Center(
-              child: SizedBox(
-                width: 120,
-                height: 45,
-                child: ElevatedButton.icon(
-                  label: const Text('Calcular'),
-                  icon: const Icon(Icons.calculate_outlined),
-                  style: ElevatedButton.styleFrom(
-                    elevation: 0,
-                    primary: Theme.of(context).colorScheme.secondary,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(12.0),
-                    ),
+    return GestureDetector(
+      onTap: () {
+        loseFocus();
+      },
+      child: Scaffold(
+          appBar: AppBar(
+            title: const Text('Calculadora Renda Fixa'),
+            actions: [
+              IconButton(
+                  icon: const Icon(
+                    Icons.settings_outlined,
                   ),
                   onPressed: () {
-                    calcularResultado();
-                  },
-                  //child:
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute<void>(
+                          builder: (BuildContext context) => PgConfigs(),
+                          fullscreenDialog: true,
+                        ));
+                  }),
+            ],
+          ),
+          body: ListView(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            shrinkWrap: true,
+            children: [
+              const SizedBox(
+                height: 20,
+              ),
+              TextField(
+                inputFormatters: <TextInputFormatter>[
+                  FilteringTextInputFormatter.allow(
+                      RegExp(r'^(\d+)?\.?\d{0,2}'))
+                ],
+                keyboardType:
+                    const TextInputType.numberWithOptions(decimal: true),
+                minLines: 1,
+                maxLines: 1,
+                maxLength: 10,
+                maxLengthEnforcement: MaxLengthEnforcement.enforced,
+                controller: valor,
+                decoration: const InputDecoration(
+                  labelText: "Valor da Aplicação",
+                  counterText: "",
+                ),
+                textAlign: TextAlign.end,
+                style: const TextStyle(
+                  fontSize: 16,
+                ),
+                onEditingComplete: () => {},
+              ),
+              SizedBox(
+                height: txtFieldSpacing,
+              ),
+              TextField(
+                inputFormatters: <TextInputFormatter>[
+                  FilteringTextInputFormatter.allow(
+                      RegExp(r'^(\d+)?\.?\d{0,2}'))
+                ],
+                keyboardType:
+                    const TextInputType.numberWithOptions(decimal: true),
+                minLines: 1,
+                maxLines: 1,
+                maxLength: 10,
+                maxLengthEnforcement: MaxLengthEnforcement.enforced,
+                controller: periodo,
+                decoration: const InputDecoration(
+                  labelText: "Período (Meses)",
+                  counterText: "",
+                ),
+                textAlign: TextAlign.end,
+                style: const TextStyle(
+                  fontSize: 16,
+                ),
+                onEditingComplete: () => {},
+              ),
+              SizedBox(
+                height: txtFieldSpacing,
+              ),
+              Row(
+                children: [
+                  Flexible(
+                    child: TextField(
+                      inputFormatters: <TextInputFormatter>[
+                        FilteringTextInputFormatter.allow(
+                            RegExp(r'^(\d+)?\.?\d{0,2}'))
+                      ],
+                      keyboardType:
+                          const TextInputType.numberWithOptions(decimal: true),
+                      minLines: 1,
+                      maxLines: 1,
+                      maxLength: 10,
+                      maxLengthEnforcement: MaxLengthEnforcement.enforced,
+                      controller: taxaDi,
+                      decoration: const InputDecoration(
+                        labelText: "DI - % ao Ano",
+                        counterText: "",
+                      ),
+                      textAlign: TextAlign.end,
+                      style: const TextStyle(
+                        fontSize: 16,
+                      ),
+                      onEditingComplete: () => {},
+                    ),
+                  ),
+                  SizedBox(
+                    width: txtFieldSpacing,
+                  ),
+                  Flexible(
+                    child: TextField(
+                      inputFormatters: <TextInputFormatter>[
+                        FilteringTextInputFormatter.allow(
+                            RegExp(r'^(\d+)?\.?\d{0,2}'))
+                      ],
+                      keyboardType:
+                          const TextInputType.numberWithOptions(decimal: true),
+                      minLines: 1,
+                      maxLines: 1,
+                      maxLength: 10,
+                      maxLengthEnforcement: MaxLengthEnforcement.enforced,
+                      controller: taxaSelic,
+                      decoration: const InputDecoration(
+                        labelText: "Selic - % ao Ano",
+                        counterText: "",
+                      ),
+                      textAlign: TextAlign.end,
+                      style: const TextStyle(
+                        fontSize: 16,
+                      ),
+                      onEditingComplete: () => {},
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: txtFieldSpacing,
+              ),
+              Row(
+                children: [
+                  Flexible(
+                    child: TextField(
+                      inputFormatters: <TextInputFormatter>[
+                        FilteringTextInputFormatter.allow(
+                            RegExp(r'^(\d+)?\.?\d{0,2}'))
+                      ],
+                      keyboardType:
+                          const TextInputType.numberWithOptions(decimal: true),
+                      minLines: 1,
+                      maxLines: 1,
+                      maxLength: 10,
+                      maxLengthEnforcement: MaxLengthEnforcement.enforced,
+                      controller: cdb,
+                      decoration: const InputDecoration(
+                        labelText: "% CDB / LC",
+                        counterText: "",
+                      ),
+                      textAlign: TextAlign.end,
+                      style: const TextStyle(
+                        fontSize: 16,
+                      ),
+                      onEditingComplete: () => {},
+                    ),
+                  ),
+                  SizedBox(
+                    width: txtFieldSpacing,
+                  ),
+                  Flexible(
+                    child: TextField(
+                      inputFormatters: <TextInputFormatter>[
+                        FilteringTextInputFormatter.allow(
+                            RegExp(r'^(\d+)?\.?\d{0,2}'))
+                      ],
+                      keyboardType:
+                          const TextInputType.numberWithOptions(decimal: true),
+                      minLines: 1,
+                      maxLines: 1,
+                      maxLength: 10,
+                      maxLengthEnforcement: MaxLengthEnforcement.enforced,
+                      controller: lciLca,
+                      decoration: const InputDecoration(
+                        labelText: "% LCI / LCA",
+                        counterText: "",
+                      ),
+                      textAlign: TextAlign.end,
+                      style: const TextStyle(
+                        fontSize: 16,
+                      ),
+                      onEditingComplete: () => {},
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(
+                height: 40,
+              ),
+              Center(
+                child: SizedBox(
+                  width: 120,
+                  height: 45,
+                  child: ElevatedButton.icon(
+                    label: const Text('Calcular'),
+                    icon: const Icon(Icons.calculate_outlined),
+                    style: ElevatedButton.styleFrom(
+                      elevation: 0,
+                      primary: Theme.of(context).colorScheme.secondary,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12.0),
+                      ),
+                    ),
+                    onPressed: () {
+                      calcularResultado();
+                    },
+                    //child:
+                  ),
                 ),
               ),
-            ),
-            const SizedBox(
-              height: 30,
-            ),
-            Text(_resultado),
-          ],
-        ));
+              const SizedBox(
+                height: 30,
+              ),
+              Text(_resultado),
+            ],
+          )),
+    );
   }
 }
--- lib/pages/util/calc_functions.dart
+++ lib/pages/util/calc_functions.dart
@@ -1,18 +1,30 @@
 import 'dart:math' as math;
 
-double calcularPoupanca(double valorAplic, double periodo){
+double calcularPoupanca(double valorAplic, double periodo) {
   return valorAplic * (math.pow(0.5 / 100 + 1, periodo)) - valorAplic;
 }
 
-double calcularLci(double valorAplic, double periodo, double di){
+num calcularLci(double valorAplic, double periodo, double di, double impRenda,
+    double porcLci) {
 
+
+  print ("valor sem desconto "+(valorAplic * (math.pow(di / periodo , periodo)) - valorAplic).toString());
+
+//Math.pow(((porcLci / 100 * di) / 100 + 1), (1 / 12));
+
+  double taxa = impRenda / 100;
+  num v = math.pow(( di / 100 + 1), periodo);
+
+  //print("v -> " + v.toString());
+  //print("v desc -> "+(v - (v * impRenda / 100)).toString());*/
+
+  return 1; //800
+}
+
+double calcularCdb(double valorAplic, double periodo) {
   return 1;
 }
 
-double calcularCdb(double valorAplic, double periodo){
-  return 1;
-}
-
-double calcularTesouro(double valorAplic, double periodo){
+double calcularTesouro(double valorAplic, double periodo) {
   return 1;
 }
